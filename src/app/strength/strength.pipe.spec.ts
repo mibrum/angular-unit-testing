71a9e1dba403c73f2f1c32f676ec3896ce8be8b0
Mireia <mireiabrumartinez@gmail.com>
2020-11-16 14:48:35 +0100

    Add "strength" pipe test

---
+++ src/app/strength/strength.pipe.spec.ts
@@ -0,0 +1,13 @@
+import { StrengthPipe } from "./strength.pipe"
+
+describe('StrengthPipe', () => {
+    let pipe = new StrengthPipe();
+
+    it('Should display weak if strength is 5', () => {
+        expect(pipe.transform(5)).toEqual('5 (weak)');
+    })
+
+    it('Should display strong if strength is 10', () => {
+        expect(pipe.transform(10)).toEqual('10 (strong)')
+    })
+})
